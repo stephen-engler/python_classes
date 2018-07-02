a0e366b499ce2777105c0efe8ed5999f4cb0a4a3
Stephen Engler <sengler1989@gmail.com>
2018-07-02 15:18:27 -0500

    add more attributes

--- classes_intro.py
+++ classes_intro.py
@@ -1,8 +1,13 @@
 class Dog():
-    def __init__(self, breed):
+    def __init__(self, breed, name, spots):
+        #attributes 
+        #we take in argument
+        #assign it using self.attribute_name
         self.breed = breed
+        self.name = name
+        self.spots = spots
 
-my_dog = Dog('test')
+my_dog = Dog('lab', 'buster', False)
 my_dog.breed
 
 
