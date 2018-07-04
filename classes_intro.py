488d9456bb69e76740c977a003abb6fe7a04c93d
Stephen Engler <sengler1989@gmail.com>
2018-07-04 15:20:19 -0500

    methods and attributes

--- classes_intro.py
+++ classes_intro.py
@@ -1,4 +1,7 @@
 class Dog():
+    #class object attribute 
+    species = 'mammal'
+
     def __init__(self, breed, name, spots):
         #attributes 
         #we take in argument
@@ -7,8 +10,15 @@ class Dog():
         self.name = name
         self.spots = spots
 
-my_dog = Dog('lab', 'buster', False)
+    #methods 
+
+    def bark(self):
+        print('woof')
+
+my_dog = Dog(breed = 'lab', name = 'buster', spots = False)
 my_dog.breed
+
+my_dog.bark()
 
 
 
